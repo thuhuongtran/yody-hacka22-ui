c1357222e2d55a1b0cc2a123c6ad3d4a2813a9d8
huong11.tranthithu <huong.tranthu.vn@gmail.com>
2022-06-04 20:59:13 +0700

    cart page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FirstCorePageComponent } from './first-core-page/first-core-page.component';
 import { DesignCardComponent } from './design-card/design-card.component';
+import { ChooseDesignDecorComponent } from './choose-design-decor/choose-design-decor.component';
+import { CartComponent } from './cart/cart.component';
+import { CartItemComponent } from './cart-item/cart-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FirstCorePageComponent,
-    DesignCardComponent
+    DesignCardComponent,
+    ChooseDesignDecorComponent,
+    CartComponent,
+    CartItemComponent
   ],
   imports: [
     BrowserModule,
