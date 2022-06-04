245413f1b5fdaa907a0405b7df1e499945d41fc2
Hùng Nguyễn Quốc <imbayonline@gmail.com>
2022-06-05 02:24:10 +0700

    change state in shirt type

--- src/app/first-core-page/first-core-page.component.ts
+++ src/app/first-core-page/first-core-page.component.ts
@@ -1,34 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import {YodyServiceService} from "../yody-service.service";
+import { YodyServiceService } from '../yody-service.service';
 
 @Component({
   selector: 'app-first-core-page',
   templateUrl: './first-core-page.component.html',
-  styleUrls: ['./first-core-page.component.scss']
+  styleUrls: ['./first-core-page.component.scss'],
 })
 export class FirstCorePageComponent implements OnInit {
+  shirtType: string = 'T-shirt';
 
-  constructor(private yodyService: YodyServiceService) { }
+  constructor(private yodyService: YodyServiceService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  changeShirtType(type: string) {
+    this.shirtType = type;
   }
 
   handleFileInput(event: any) {
     let file: File = event.target.files[0];
     let formData: FormData = new FormData();
     formData.append('file', file, file.name);
-    this.yodyService.uploadFile(formData).subscribe(
-      (response) => {
-        console.log(response)
-      }
-    );
+    this.yodyService.uploadFile(formData).subscribe((response) => {
+      console.log(response);
+    });
   }
 
   getDesigns(tags: string[]) {
-    this.yodyService.searchDesignsByTags(tags).subscribe(
-      (response) => {
-        console.log(response)
-      }
-    )
+    this.yodyService.searchDesignsByTags(tags).subscribe((response) => {
+      console.log(response);
+    });
   }
 }
