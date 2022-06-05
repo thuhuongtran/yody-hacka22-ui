5da988e392fab1211440e23bfedc1392d97e9946
Hùng Nguyễn Quốc <imbayonline@gmail.com>
2022-06-05 07:53:41 +0700

    change

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
-
-  constructor() { }
+  constructor(private dataRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.dataRoute.paramMap.subscribe(() => {
+      console.log(window.history.state);
+    });
   }
-
 }
--- src/app/first-core-page/first-core-page.component.ts
+++ src/app/first-core-page/first-core-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { YodyServiceService } from '../yody-service.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-first-core-page',
@@ -9,26 +9,54 @@ import { Router } from '@angular/router';
 })
 export class FirstCorePageComponent implements OnInit {
   shirtType: string = 'T-shirt';
+  shirtColor: string = 'white';
+  yourDesign: any;
+  imageOutput: any;
 
-  shirt: {};
+  firstPage = {
+    shirtType: 'T-shirt',
+    shirtColor: 'white',
+    yourDesign: null,
+  };
 
-  constructor(private yodyService: YodyServiceService, private router: Router) {
-    this.shirt = { title: 'test' };
+  constructor(
+    private yodyService: YodyServiceService,
+    private dataRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.dataRoute.paramMap.subscribe(() => {
+      this.firstPage = window.history.state;
+    });
   }
 
-  ngOnInit(): void {}
-
   changeShirtType(type: string) {
-    this.shirtType = type;
+    this.firstPage.shirtType = type;
+  }
+
+  changeShirtColor(color: string) {
+    this.firstPage.shirtColor = color;
   }
 
   handleFileInput(event: any) {
-    let file: File = event.target.files[0];
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+      this.yourDesign = event.target.files[0];
+      reader.readAsDataURL(this.yourDesign);
+
+      reader.onload = (event) => {
+        this.imageOutput = event.target?.result;
+      };
+    }
     let formData: FormData = new FormData();
-    formData.append('file', file, file.name);
-    this.yodyService.uploadFile(formData).subscribe((response) => {
-      console.log(response);
-    });
+    formData.append('file', this.yourDesign, this.yourDesign.name);
+
+    // TODO: GET file URL or Base64 to store to object
+
+    // this.yodyService.uploadFile(formData).subscribe((response) => {
+    //   console.log(response);
+    //   this.firstPage.yourDesign = response.xx;
+    // });
   }
 
   getDesigns(tags: string[]) {
